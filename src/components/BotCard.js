079843310c6c8b8c3a08417b2ee161ddd7541029
Christopher Kim <chris.kim1990@gmail.com>
2021-05-26 12:06:11 -0400

    submission for deadline

--- src/components/BotCard.js
+++ src/components/BotCard.js
@@ -9,46 +9,45 @@ const botTypeClasses = {
   Captain: "icon star"
 };
 
-const BotCard = props => {
+const BotCard = ({bot, handleClick, handleDelete}) => {
   return (
     <div className="ui column">
       <div
         className="ui card"
-        key={props.bot.id}
-        onClick={() => console.log("add code to connect event listener")}
-      >
-        <div className="image">
-          <img alt="oh no!" src={props.bot.avatar_url} />
+        key={bot.id}
+        >
+        <div className="image" onClick={() => handleClick(bot)}>
+          <img alt="oh no!" src={bot.avatar_url} />
         </div>
         <div className="content">
           <div className="header">
-            {props.bot.name}
-            <i className={botTypeClasses[props.bot.bot_class]} />
+            {bot.name}
+            <i className={botTypeClasses[bot.bot_class]} />
           </div>
           <div className="meta text-wrap">
-            <small>{props.bot.catchphrase}</small>
+            <small>{bot.catchphrase}</small>
           </div>
         </div>
         <div className="extra content">
           <span>
             <i className="icon heartbeat" />
-            {props.bot.health}
+            {bot.health}
           </span>
 
           <span>
             <i className="icon lightning" />
-            {props.bot.damage}
+            {bot.damage}
           </span>
           <span>
             <i className="icon shield" />
-            {props.bot.armor}
+            {bot.armor}
           </span>
           <span>
             <div className="ui center aligned segment basic">
               <button
                 className="ui mini red button"
                 onClick={() =>
-                  console.log("add code to connect event listener")
+                  handleDelete(bot)
                 }
               >
                 x
--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -1,10 +1,63 @@
 import React, { Component } from "react";
+import BotCollection from './BotCollection'
+import YourBotArmy from "./YourBotArmy";
+
+const botsUrl = 'http://localhost:6001/bots'
+const headers = {
+  Accepts: 'application/json',
+  'Content-type': 'application/json'
+}
 
 class BotsPage extends Component {
   //start here with your code for step one
+  state = {
+    bots: [],
+    botArmy: []
+  }
+  //gets all bots
+  componentDidMount(){
+    fetch(botsUrl)
+      .then(res => res.json())
+      .then((bots) => this.setState({bots}))
+  }
+  //adds to bot army
+  botArmy = (bot) => {
+    if(!this.state.botArmy.includes(bot)){
+      this.setState({
+        botArmy: [...this.state.botArmy, bot]
+      })
+    }
+  }
+
+  releaseBot = (bot) => {
+    this.setState({ botArmy: this.state.botArmy.filter((b) => b !== bot)})
+  }
+
+  deleteBot = (bot) => {
+    this.releaseBot(bot)
+    fetch(`${botsUrl}/${bot.id}`, {
+      method: 'DELETE',
+      headers,
+    })
+      .then(() => {
+        this.setState({bots: this.state.bots.filter((b) => b.id !== bot.id)})
+      })
+      .catch((err) => console.log(err))
+  }
 
   render() {
-    return <div>{/* put your components here */}</div>;
+    return <div>
+      <YourBotArmy
+        bots={this.state.botArmy}
+        handleClick={this.releaseBot}
+        handleDelete={this.deleteBot}
+      />
+      <BotCollection
+        bots={this.state.bots}
+        handleClick={this.botArmy}
+        handleDelete={this.deleteBot}
+      />
+    </div>;
   }
 }
 
